b19a698edcc75431ebd149bf25872595582fd217
jimliuxinghai <jimliuxinghai@163.com>
2018-05-08 18:29:18 +0800

    'production-db'

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,13 @@
 const port = Number.parseInt(process.env.PORT) || 9050;
 
 export default {
-  port: port
+  port: port,
+  database: {
+    DATABASE: 'hunter',
+    USERNAME: 'join_web',
+    PASSWORD: 'mysql_joinwisdom',
+    PORT: '3306',
+    HOST: '10.6.5.137',
+    dateStrings: 'DATE'
+  }
 }
