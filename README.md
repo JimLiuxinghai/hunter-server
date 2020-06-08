d62dcb6b6dcb4b0ab5aebff939aeacb3e1d31916
jimliuxinghai <jimliuxinghai@163.com>
2020-06-08 12:30:59 +0800

    modify:readme

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # hunter-server
 
 server for hunter
+
+
+## 初始化数据库
+
+1.config/development.js中配置数据库信息
+
+2. `npm run init_sql`
+
+3. `yarn`
+
+4. 开发环境启动  `npm run dev`
--- config/env/test.js
+++ config/env/test.js
@@ -9,5 +9,12 @@ export default {
         PORT: '3306',
         HOST: '127.0.0.1',
         dateStrings: 'DATE'
+    },
+    redis: {
+        port: 6379, // Redis port
+        host: "127.0.0.1", // Redis host
+        family: 4, // 4 (IPv4) or 6 (IPv6)
+        password: "auth",
+        db: 0
     }
 }
